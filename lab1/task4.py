db11c2cbe35745d7b6bc429300096d0d95c1f0d6
ZaiiiRan <a1b075753@gmail.com>
2025-09-18 20:56:04 +0300

    L1 T4 video writer

---
+++ lab1/task4.py
@@ -0,0 +1,20 @@
+import cv2
+
+cap = cv2.VideoCapture('./videos/cute_cat.mp4')
+
+width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('./videos/new_cat.avi', fourcc, 30, (width, height))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        out.write(frame)
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()

c07859fa2d49882d8fc226081efdb0d2608ee32b
ZaiiiRan <a1b075753@gmail.com>
2025-10-16 04:13:16 +0300

    L2 T2 red mask

--- lab2/main.py
+++ lab2/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
@@ -9,7 +10,11 @@ while True:
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow("Red tracker", hsv)
+    mask1 = cv2.inRange(hsv, np.array([0, 100, 100]), np.array([20, 255, 255]))
+    mask2 = cv2.inRange(hsv, np.array([160, 100, 100]), np.array([180, 255, 255]))
+    mask = mask1 | mask2
+
+    cv2.imshow("Red tracker", cv2.bitwise_and(frame, frame, mask=mask))
 
     if cv2.waitKey(1) & 0xFF == 27:
         break

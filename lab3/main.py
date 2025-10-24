dd987dde6789a31e8ffbfaeb4323e4b2cdd9940f
ZaiiiRan <a1b075753@gmail.com>
2025-10-24 12:13:03 +0300

    L3 T2 norm kernel

--- lab3/main.py
+++ lab3/main.py
@@ -15,6 +15,9 @@ def build_kernel(size, sigma):
     
     return matr
 
+def norm_kernel(kernel):
+    return kernel / kernel.sum()
+
 kernel3 = build_kernel(3, blur_parameter)
 kernel5 = build_kernel(5, blur_parameter)
 kernel7 = build_kernel(7, blur_parameter)
@@ -22,3 +25,11 @@ kernel7 = build_kernel(7, blur_parameter)
 print('Ядро свертки 3x3: ', kernel3)
 print('Ядро свертки 5x5: ', kernel5)
 print('Ядро свертки 7x7: ', kernel7)
+
+kernel3 = norm_kernel(kernel3)
+kernel5 = norm_kernel(kernel5)
+kernel7 = norm_kernel(kernel7)
+
+print('Нормированное ядро свертки 3x3: ', kernel3)
+print('Нормированное ядро свертки 5x5: ', kernel5)
+print('Нормированное ядро свертки 7x7: ', kernel7)
